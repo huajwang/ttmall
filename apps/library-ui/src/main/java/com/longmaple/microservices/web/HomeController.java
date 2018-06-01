321554a9069786d0cd04f2017305a833084f7416
Huajian Wang <huajian.wang@nuance.com>
2018-06-01 07:48:31 -0400

    Change JWT resource server;

--- apps/library-ui/src/main/java/com/longmaple/microservices/web/HomeController.java
+++ apps/library-ui/src/main/java/com/longmaple/microservices/web/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String index() {
-		return "/public/index";
+		return "index";
 	}
 }
--- apps/resource-server/src/main/java/com/longmaple/oauth2/controller/AddressController.java
+++ apps/resource-server/src/main/java/com/longmaple/oauth2/controller/AddressController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class AddressController {
 	@Autowired
 	private AddressRepo addrRepo;
 
+	@CrossOrigin
 	@GetMapping("/address")
 	@PreAuthorize("#oauth2.hasScope('user:profile:read')")
 	public ResponseEntity<Address> getAddress(Authentication auth) {
