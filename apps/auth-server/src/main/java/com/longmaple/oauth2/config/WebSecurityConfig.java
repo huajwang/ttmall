22f6f81ab6c78175ea7fe85f235702bae69a620d
Wang <huajian_wang@nuance.com>
2018-06-02 11:39:12 -0400

    allow login form to be presented without authentication needed; This solve 'user need to authentication first issue'

--- apps/auth-server/src/main/java/com/longmaple/oauth2/config/WebSecurityConfig.java
+++ apps/auth-server/src/main/java/com/longmaple/oauth2/config/WebSecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
@@ -16,14 +18,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()            
-		.requestMatchers().antMatchers("/oauth/revoke")
-		.and()            
-		.httpBasic()
-		.and()            
-		.authorizeRequests()
-		.antMatchers("/register").permitAll()
-		.anyRequest().authenticated();
+		 http
+		 .antMatcher("/**").authorizeRequests()
+		 .antMatchers("/", "/login**")
+		 .permitAll()
+		 .anyRequest().authenticated().and().formLogin().permitAll();
 	}
 	
 }
