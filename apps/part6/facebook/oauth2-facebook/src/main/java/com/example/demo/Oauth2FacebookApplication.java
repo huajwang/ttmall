3551964b44effe739c5b91d54e430fb9309afade
Wang <huajian_wang@nuance.com>
2018-06-02 11:21:56 -0400

    social oauth2 - facebook

---
+++ apps/part6/facebook/oauth2-facebook/src/main/java/com/example/demo/Oauth2FacebookApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2FacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2FacebookApplication.class, args);
+	}
+}
